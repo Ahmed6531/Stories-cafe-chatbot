cc45efd96d43c7027ebd299f49ec038d44b1bcdf
lana <lanasalem020@gmail.com>
2026-03-17 01:31:35 +0200

    changed api to uppercase in main due to hardware issues

--- chatbot/app/main.py
+++ chatbot/app/main.py
@@ -1,8 +1,8 @@
 # app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.chat import router as chat_router
-from app.api.voice import router as voice_router
+from app.API.chat import router as chat_router
+from app.API.voice import router as voice_router
 from app.core.config import settings
 
 app = FastAPI(title=settings.app_name, version="0.1.0")
